d9c78f2ed29564fd7afb70f27ee787a7cc3da7bd
Sid775 <sid775@mail.ru>
2017-03-18 16:50:24 +0300

    final version

--- web/scripts/index.js
+++ web/scripts/index.js
@@ -8,7 +8,7 @@ for (var i=0; i<$allButtons.length; i++){
 }
 
 function buttonClick(){
-    if (this.value == "=") {
+    /*if (this.value == "=") {
         document.getElementById("display").value = eval(document.getElementById("display").value).toString(8);
     }else if (this.value == "c"){
         document.getElementById("display").value = "";
@@ -16,6 +16,32 @@ function buttonClick(){
         document.getElementById("display").value = document.getElementById("display").value.substr(0, document.getElementById("display").value.length -1);
     }else{
         document.getElementById("display").value += this.value;
+    }*/
+    if (!isNaN(this.value)){
+        document.getElementById("display").value += this.value.toString(8);
+    }else if (this.value == "="){
+        if (operationMemory == "+"){
+            document.getElementById("display").value = (+memory.toString(10) + +document.getElementById("display").value.toString(10)).toString(8);
+        }else
+        if (operationMemory == "-"){
+            document.getElementById("display").value = (+memory.toString(10) - +document.getElementById("display").value.toString(10)).toString(8);
+        }else
+        if (operationMemory == "*"){
+            document.getElementById("display").value = (+memory.toString(10) * +document.getElementById("display").value.toString(10)).toString(8);
+        }else
+        if (operationMemory == "/"){
+            document.getElementById("display").value = (+memory.toString(10) / +document.getElementById("display").value.toString(10)).toString(8);
+        }
+    }else if (this.value == "+" || this.value == "-" || this.value == "*" || this.value == "/"){
+        memory = document.getElementById("display").value;
+        operationMemory = this.value;
+        document.getElementById("display").value = "";
+    }else if (this.value == "c"){
+        memory = "";
+        operationMemory = "";
+        document.getElementById("display").value = "";
+    }else if (this.value == "<-"){
+        document.getElementById("display").value = document.getElementById("display").value.substr(0, document.getElementById("display").value.length -1);
     }
 }
 
